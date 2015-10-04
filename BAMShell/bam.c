41a1e41f30184f24189793b305140ad2b20c40d8
Augusto Meza <amp_pumas@hotmail.com>
2015-10-03 20:40:41 -0500

    Implementación de un historial mediante el uso de un thread, la variable changed indica una linea nueva que debe volcarse en el historial.

--- BAMShell/bam.c
+++ BAMShell/bam.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include <sys/wait.h>
+#include <pthread.h>
 
 #define MAX_ARGS 64
 #define LIMITADORES " \t\r\n\a"
 
+FILE *bitacora;
+char *linea;
+int estado=1,changed=0;
+pthread_t his_thread;
+pthread_mutex_t muthis = PTHREAD_MUTEX_INITIALIZER;
 int micd(char **args);
 int miayuda(char **args);
 int misalida(char **args);
-
 char *nuestros[] = {"cd","ayuda","salir"};
 int (*funciones[]) (char **) = {&micd,&miayuda,&misalida};
+
 int cuantasFunciones()
 {
 	return sizeof(nuestros)/sizeof(char*);
@@ -31,7 +38,7 @@ int miayuda(char **args)
 {
 	int i;
 	printf("\t\tBAM Shell\n");
-	printf("Programado por Augusto Meza y Rosa Maria Yolotzin Muñoz Alvarez\n");
+	printf("Programado por Augusto Meza Peña y Rosa Maria Yolotzin Muñoz Alvarez\n");
 	printf("Con codigo de Stephen Brennan's LSH https://github.com/brenns10/lsh\n");
 	printf("Comandos del Shell\n");
 	for (i = 0; i < cuantasFunciones(); i++)
@@ -51,6 +58,8 @@ char **parseo(char *linea)
 	int pos = 0;
 	char **tokens = malloc(MAX_ARGS * sizeof(char *));
 	char *token;
+	changed=1;
+	do{}while(changed);
 	if(!tokens)
 	{
 		fprintf(stderr, "bam: no se pudo reservar memoria\n");
@@ -75,13 +84,17 @@ char **parseo(char *linea)
 int correr(char **linea_parse)
 {
 	int estado,pid;
+	fclose(bitacora);
 	if((pid = fork())==0)
 	{
 		execvp(linea_parse[0], linea_parse);
 		perror("bam");
+		exit(0);
 	}
-	else if(pid<0)	perror("bam");
+	else if(pid<0)	perror("bam"),exit(0);
 	else	waitpid(pid,&estado,WUNTRACED);
+	bitacora = fopen("bitacora.txt","a");
+	if(!bitacora)	perror("bam");
 	return 1;
 }
 
@@ -100,11 +113,8 @@ int ejecutar(char **linea_parse)
 
 void loop()
 {
-	char *linea;
 	char **linea_parse;
 	size_t lon = 0;
-	int estado;
-
 	do {
 		printf("¬ ");
 		linea = (char*)NULL;
@@ -116,9 +126,32 @@ void loop()
 	}while(estado);
 }
 
+void *histhread(void *args)
+{
+	time_t rawtime;
+ 	struct tm * timeinfo;
+	time(&rawtime);
+	timeinfo=localtime(&rawtime);
+	bitacora = fopen("bitacora.txt","a");
+	if(!bitacora)	perror("bam");
+	fprintf (bitacora,"Fecha y Hora: %s", asctime(timeinfo));
+	do{
+		if(changed)
+		{
+			fprintf(bitacora, "%s", linea);
+			changed=0;		
+		}
+	}while(estado);
+	fclose(bitacora);
+}
+
 int main(int argc, char const *argv[])
 {
+	if(pthread_create(&his_thread, NULL, histhread, NULL))
+		perror("bam");
 	system("clear");
 	loop();
+	if(pthread_join(his_thread, NULL))
+		perror("bam");
 	return 0;
 }
