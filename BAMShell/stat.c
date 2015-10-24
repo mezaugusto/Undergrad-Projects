289ac1f6761445196b4d70c57f31859a59cbef83
Augusto Meza <amp_pumas@hotmail.com>
2015-10-24 18:55:58 -0500

    Stat bien hecho

--- BAMShell/stat.c
+++ BAMShell/stat.c
@@ -1,45 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <time.h>
+#include <pwd.h>
+#include <grp.h>
 #include <fcntl.h>
+
 int main(int argc, char const *argv[])
 {
 	if(argc!=2)	perror("stat");
 	int file;
+	char modo[7];
 	struct stat st;
+	struct group *grp;
+	struct passwd *pwd;
+	static const char *rwx[] = {"---", "--x", "-w-",
+	"-wx","r--","r-x","rw-","rwx"};
+    static char bits[11];
 	if (stat(argv[1], &st) == -1)	perror("stat");
-   	switch (st.st_mode & S_IFMT) {
-    case S_IFBLK:  printf("b"); break;
-    case S_IFCHR:  printf("c"); break;
-    case S_IFDIR:  printf("d"); break;
-    case S_IFIFO:  printf("p"); break;
-    case S_IFLNK:  printf("l"); break;
-    case S_IFREG:  printf("-"); break;
-    case S_IFSOCK: printf("s");	break;
-    default:       printf("u");	break;
+	printf("  File: \"%s\"\n", argv[1]);
+	printf("  Size: %lld\tFile Type: ", (long long) st.st_size);
+   	switch (st.st_mode & S_IFMT)
+   	{
+	    case S_IFBLK:  printf("Special Block File\n"); break;
+	    case S_IFCHR:  printf("Character\n"); break;
+	    case S_IFDIR:  printf("Directory\n"); break;
+	    case S_IFIFO:  printf("Pipe or FIFO\n"); break;
+	    case S_IFLNK:  printf("Symbolic Link"); break;
+	    case S_IFREG:  printf("Regular File\n"); break;
+	    case S_IFSOCK: printf("Socket\n");	break;
+	    default:       printf("Unknown\n");	break;
     }
-    printf("I-node number:            %ld\n", (long) st.st_ino);
-
-   printf("Mode:                     %lo (octal)\n",
-            (unsigned long) st.st_mode);
-
-   printf("Link count:               %ld\n", (long) st.st_nlink);
-    printf("Ownership:                UID=%ld   GID=%ld\n",
-            (long) st.st_uid, (long) st.st_gid);
-
-   printf("Preferred I/O block size: %ld bytes\n",
-            (long) st.st_blksize);
-    printf("File size:                %lld bytes\n",
-            (long long) st.st_size);
-    printf("Blocks allocated:         %lld\n",
-            (long long) st.st_blocks);
-
-   printf("Last status change:       %s", ctime(&st.st_ctime));
-    printf("Last file access:         %s", ctime(&st.st_atime));
-    printf("Last file modification:   %s", ctime(&st.st_mtime));
-
-
-    printf("\n");
+    snprintf(modo,sizeof(modo),"%lo", (unsigned long) st.st_mode);
+    modo[0]=' ';
+    modo[1]='(';
+    printf("  Mode:%s/",modo);
+    strcpy(&bits[1], rwx[(st.st_mode >> 6)& 7]);
+    strcpy(&bits[4], rwx[(st.st_mode >> 3)& 7]);
+    strcpy(&bits[7], rwx[(st.st_mode & 7)]);
+    if (st.st_mode & S_ISUID)
+        bits[3] = (st.st_mode & S_IXUSR) ? 's' : 'S';
+    if (st.st_mode & S_ISGID)
+        bits[6] = (st.st_mode & S_IXGRP) ? 's' : 'l';
+    if (st.st_mode & S_ISVTX)
+        bits[9] = (st.st_mode & S_IXUSR) ? 't' : 'T';
+    bits[10] = '\0';
+    switch (st.st_mode & S_IFMT)
+    {
+	    case S_IFBLK:  bits[0]='b'; break;
+	    case S_IFCHR:  bits[0]='c'; break;
+	    case S_IFDIR:  bits[0]='d'; break;
+	    case S_IFIFO:  bits[0]='p'; break;
+	    case S_IFLNK:  bits[0]='l'; break;
+	    case S_IFREG:  bits[0]='-'; break;
+	    case S_IFSOCK: bits[0]='s';	break;
+	    default:       bits[0]='u';	break;
+	}
+	printf("%s)\t",bits);
+	printf("Uid:(%4ld/%s)\t", (long) st.st_uid,
+		getpwuid(st.st_uid)->pw_name);
+	printf("Gid:(%4ld/%s)\n", (long) st.st_gid,
+		getgrgid(st.st_gid)->gr_name);
+    printf("Device: %d,%d\t", 
+    	(int)(((unsigned int) (st.st_dev) >> 8) & 0xff),
+    	(int)((st.st_dev) & 0xff));
+    printf("Inode: %ld\t", (long) st.st_ino);
+	printf("Links: %ld\n", (long) st.st_nlink);
+    printf("Access: %s", ctime(&st.st_atime));
+    printf("Modify: %s", ctime(&st.st_mtime));
+	printf("Change: %s", ctime(&st.st_ctime));
 	return 0;
 }
