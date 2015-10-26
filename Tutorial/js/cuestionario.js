ec814330fdfca2058f4c6ce525f16fc39cead141
Augusto Meza <amp_pumas@hotmail.com>
2015-10-25 20:07:23 -0600

    Inicio del Tutorial de IS

---
+++ Tutorial/js/cuestionario.js
@@ -0,0 +1,238 @@
+preguntas=[
+["¿En qué consiste hacer una prueba en un sistema de software?",
+"Es una revisión para conocer si se ha implementado el programa correctamente.",
+"En conocer que tan probable es que ocurra un error cualquiera.",
+"Se obtiene información de las salidas obtenidas al ingresar información aleatoria.",
+"Se revisa una función en específico para conocer cuanto tiempo tarda en realizarla el sistema."],
+
+["¿Por qué cuesta trabajo conocer qué tan fiable es un sistema crítico?",
+"Por los altos costos de probar y analizar el sistema.",
+"Porque los métodos estadísticos son muy largos.",
+"Las pruebas son muy difíciles de realizar y de diseñar.",
+"Por lo grande que es el código de un sistema crítico."],
+
+["¿Cuál es la prueba más importante para conocer el nivel de seguridad de un sistema?",
+"Contradicción.",
+"Integración.",
+"Funcional.",
+"Desempeño."],
+
+["¿Qué es la validación?",
+"Actividades dirigidas a asegurar que el sistema siempre cumplirá los requisitos del cliente.",
+"Es un análisis del diseño del sistema para conocer que mejoras se pueden realizar.",
+"Detectar que carencias tiene el sistema de una empresa para poder mejorarlo.",
+"Es ingresar datos aleatorios al sistema para encontrar posibles errores que puedan ocurrir."],
+
+["¿Qué características tiene la depuración?",
+"Pruebas estadísticas y especificación formal para solucionar errores encontrados.", 
+"Pruebas estadísticas y protección.",
+"Eliminación de errores y reparación.",
+"Especificaciones formales y programación."],
+
+["¿Qué técnicas se pueden usar en la validación de la protección?",
+"Equipos tigre y validación con herramientas.",
+"Ingreso de los mismos datos muchas veces y equipos tigre.",
+"Permanencia de la base de datos y respaldo en la información.",
+"Inyección de SQL y perfiles operacionales."],
+
+["¿Qué es un sistema crítico?",
+"Sistema en el cual una falla puede tener consecuencias graves, incluyendo la muerte de otras personas.",
+"Es un sistema que tiene cambios muy drásticos en su funcionamiento.",
+"Sistema creado especificamente para el gobierno, en el cual hay información importante del país.",
+"Sistema que no puede mejorarse más y no necesita ningún tipo de mantenimiento."],
+
+["En una función escalonada ¿cómo es el comportamiento de la fiabilidad?",
+"Varía dentro de un rango.",
+"Constante o nulo.",
+"Decreciente.",
+"No es posible determinar la fiabilidad de un sistema crítico."],
+
+["¿Cuándo se repara un fallo en un sistema crítico?",
+"En el momento que se descubren.",
+"En la siguiente sesión de mantenimiento.",
+"Cuando se regresa al momento de rediseñar el sistema.",
+"No se reparan, sirve para documentar el progreso del sistema."],
+
+["¿Qué modelo se acopla más al desarrollo de un sistema crítico?",
+"Cascada.",
+"Prototipo.",
+"Espiral.",
+"Componentes."],
+
+["¿Qué es un método formal?",
+"Técnicas matemáticas usadas para describir las propiedades del sistema.",
+"Maneras de desarrollar un sistema siguiendo un orden predeterminado.",
+"Es un método para asegurar que un sistema sea crítico al analizar los requerimientos.",
+"Es un método que se aplica a tiempo para evitar un error mientras se ejecute el sistema."],
+
+["¿Cómo se puede descubrir inconsistencias en las especificaciones de un sistema crítico?",
+"Utilizando métodos formales para analizar las especificaciones.",
+"Validando la fiabilidad del sistema al completarlo.",
+"Realizando prototipos para que el cliente verifique que todo funciona.",
+"Utilizando equipos tigre para descubrir problemas de protección."],
+
+["¿Por qué los métodos formales no son siempre útiles?",
+"No se puede modelar todos los elementos involucrados en el sistema para su análisis.",
+"Es muy alta la probabilidad de cometer errores en estos métodos.",
+"No son muy conocidos y cuesta trabajo realizarlos.",
+"No se puede analizar las especificaciones de un sistema crítico con estos métodos."],
+
+["¿Cuántas etapas tiene la validación de la fiabilidad en un sistema crítico?",
+"4.",
+"2.",
+"8.",
+"10."],
+
+["¿Qué relaciona un perfil operacional?",
+"Entradas y la frecuencia de las mismas.",
+"Entradas y el número de salidas que puede generar el sistema.",
+"Etapas que debe pasar el sistema para procesar una instrucción.",
+"Las diferentes operaciones del sistema con la fiabilidad del mismo."],
+
+["¿Cómo cambia la fiabilidad al reparar un error?",
+"La fiabilidad puede incrementarse, pero también disminuir por los nuevos errores.",
+"Es constante, ya que la fiabilidad no depende del número de errores en el sistema.",
+"Siempre aumenta, cada error eliminado asegura que el sistema trabajará mejor.",
+"Siempre disminuye, cada error corregido genera nuevos errores."],
+
+["Selecciona un beneficio que brinda predecir el cambio en la fiabilidad durante el desarrollo de un sistema.",
+"Negociar con los clientes las especificaciones si se espera un crecimiento lento de la fiabilidad.",
+"Se puede cambiar la fecha de entrega del proyecto para entregarlo mucho antes.",
+"Conocer cuando el sistema dejará de tener errores.",
+"Iniciar la planeación de como se documentará el sistema y elegir que partes del sistema se mencionarán."],
+
+["¿Cómo se puede medir la seguridad de un sistema?",
+"No es posible hacerlo cuantitativamente.",
+"Contar los ataques exitosos realizados por cada equipo tigre.",
+"Datos ingresados al sistema por unidad de tiempo.",
+"Identificar los errores encontrados por cada prueba."],
+
+["¿Cuántos pasos tiene el modelo de desarrollo de sistemas de seguridad críticos?",
+"5.",
+"2.",
+"10.",
+"9."],
+
+["¿Por qué no es posible simular los accidentes en las pruebas?",
+"La dificultad de conocer y simular todos los posibles accidentes posibles.",
+"Porque son muy caros y no conviene gastar el dinero en ese tipo de pruebas.",
+"El sistema no aguantaría ese tipo de pruebas y podría causar daño a equipo.",
+"Las simulaciones no ayudan a mejorar el sistema."],
+
+["¿Qué es una aserción?",
+"Es una expresión en un programa que siempre se debe cumplir.",
+"Significa que el sistema es totalmente seguro y fiable.",
+"Es un error que ocurre cuando se trata de tener un acceso no autorizado al sistema.",
+"Es una excepción que se genera cuando el sistema esta en prueba."],
+
+["¿Qué tipo de problemas son evitados con la protección?",
+"Ataques intencionales para generar daños en el sistema.",
+"Errores en la forma que el sistema procesa los datos.",
+"Solo se enfoca en problemas en los que la vida de las personas corre peligro.",
+"Asegura la permanencia de los datos en el sistema."],
+
+["¿Qué es un equipo tigre?",
+"Equipo de personas encargadas de probar la protección de un sistema.",
+"Son los desarrolladores del sistema.",
+"Una computadora en la cual se almacena el sistema, este debe ser rápido para evitar atrasos.",
+"Son piezas de hardware vitales para el sistema."],
+
+["¿Qué es la verificación?",
+"Es un conjunto de actividades que asegura la correcta implementación de una función.",
+"Es tener un soporte legal donde se indica que nuestro sistema sirve.",
+"Es el conjunto de pruebas finales que se le hace al sistema.",
+"La creación correcta de las actividades para la validación."],
+
+["¿Cómo se puede comprobar la protección?",
+"La experiencia de ataques ocurridos anteriormente en otros sistemas.",
+"Gráficas para conocer cuantos errores ocurren por día.",
+"Perfil operacional de otros sistemas.",
+"Pruebas realizadas por el cliente."],
+
+["¿Qué es una prueba unitaria?",
+"Es una prueba donde solo se verifica un componente.",
+"Solo se ingresa un tipo de dato al sistema.",
+"Una prueba con duración de un minuto.",
+"Se realiza todas las pruebas en una sola sesión."],
+
+["¿Qué propiedad indica que tan probable es que sucedan accidentes en un sistema crítico?",
+"Seguridad.",
+"Protección.",
+"Disponibilidad.",
+"Exactitud."],
+
+["¿Qué es lo primero que se debe hacer para medir la fiabilidad del sistema?",
+"Identificar perfiles operacionales.",
+"Generar datos de entrada.",
+"Realizar pruebas al sistema.",
+"Realizar pruebas unitarias al sistema."],
+
+["¿Cuál de estas pruebas sirve específicamente para verificar la protección?",
+"Equipos tigre.",
+"Pruebas unitarias.",
+"Verificación formal.",
+"Perfiles operacionales."],
+
+["¿Qué consecuencia es común de un error en un sistema crítico?",
+"Pérdidas grandes de dinero.",
+"Cambio de modelo en cascada a un modelo de depuración extensiva.",
+"Falta de comunicación en los usuarios.",
+"Pérdida del servidor donde se aloja el sistema."]];
+
+//Programa creado por Guillermo Pérez Villarreal para el tutorial 7.3 en el semestre 2015-2
+//No copiar para usarlo en tutoriales futuros, solo usar como referencia al crear su cuestionario
+$(document).ready(function () {
+	$("#calificacion").hide();
+	seleccionadas=[];
+	contador=0;
+	while(contador<10)
+	{
+		numero=Math.floor(Math.random()*(29-0+1))+0;
+		if(seleccionadas.indexOf(numero)==-1)
+		{
+			seleccionadas.push(numero);
+			contador+=1;
+		}
+	}
+	
+	respuestas=[];
+	for(i=0;i<10;i++)
+	{
+		document.getElementById("h"+i).innerHTML=(i+1)+".-"+preguntas[seleccionadas[i]][0];
+		numero=Math.floor(Math.random()*(4))+1;
+		respuestas.push(numero);
+		for(j=0;j<4;j++)
+		{
+			document.getElementById("l"+i+j).innerHTML=preguntas[seleccionadas[i]][numero];
+			numero+=1;
+			if(numero==5)
+			{
+				numero=1;
+			}
+		}
+	}
+});
+
+function evaluar()
+{
+	$("#preguntas").hide("slow");
+	$("#calificacion").show("slow");
+	
+	puntuacion=0;
+	
+	for(i=0;i<10;i++)
+	{
+		correcta=respuestas[i]
+		if(correcta!=1)
+		{
+			correcta=6-correcta;
+		}
+		correcta-=1;
+		if(document.getElementById("r"+i+correcta).checked)
+		{
+			puntuacion+=1;
+		}
+	}
+	
+	document.getElementById("puntuacion").innerHTML="La puntuación obtenida es: "+puntuacion;
+}
