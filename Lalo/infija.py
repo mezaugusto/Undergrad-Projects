b5de7443535929ada0e603caa95cb3de61ad3888
Augusto Meza <amp_pumas@hotmail.com>
2016-03-12 20:51:54 -0600

    Cambios minimos ultimo programa

--- Lalo/infija.py
+++ Lalo/infija.py
@@ -6,15 +6,15 @@ def operacion(op1, op2, operador):
     elif operador == '/':
         return op2/op1
     elif operador == '%':
-        return op2%op1
+        return op2 % op1
     elif operador == '*':
         return op2*op1
-    raise TypeError("El operando"+operador+" no existe")
+    raise TypeError
 
 
 def aritmetica(cadena):
-    if cadena.replace("(","").replace(")","").isdigit():
-        return int(cadena.replace("(","").replace(")",""))
+    if cadena.replace("(", "").replace(")", "").isdigit():
+        return int(cadena.replace("(", "").replace(")", ""))
     parentesis = 0
     operadores = []
     operandos = []
@@ -24,7 +24,7 @@ def aritmetica(cadena):
         elif letter == ')':
             parentesis -= 1
             if parentesis < 0:
-                raise TypeError("Expresion mal formada")
+                raise TypeError
             operandos.append(operacion(operandos.pop(), operandos.pop(), operadores.pop()))
         elif letter.isdigit():
             operandos.append(int(letter))
@@ -33,17 +33,7 @@ def aritmetica(cadena):
     return operandos.pop()
 
 
-def booleana(cadena):
-    if cadena == 'V':
-        return True
-    elif cadena == 'F':
-        return False
-    expresion = list(cadena.replace("<=","<"))
-    if expresion.pop(0) != "(" or expresion.pop() != ")":
-        raise TypeError("Expresion mal formada")
-    letter = expresion.pop(0)
-    if letter == '¬':
-        return not booleana(''.join(expresion))
+def agrupar(expresion, letter):
     subcadena = ''
     parentesis = 0
     while parentesis != 0 or len(subcadena) == 0:
@@ -52,18 +42,23 @@ def booleana(cadena):
         elif letter == "(":
             parentesis += 1
         subcadena += letter
-        letter = expresion.pop(0)
-    simbolo = letter
-    subcadena2 = ''
-    parentesis = 0
+        letter = expresion.pop(0) if len(expresion) > 0 else None
+    return subcadena, letter
+
+
+def booleana(cadena):
+    if cadena == 'V':
+        return True
+    elif cadena == 'F':
+        return False
+    expresion = list(cadena.replace("<=", "<"))
+    if expresion.pop(0) != "(" or expresion.pop() != ")":
+        raise TypeError
     letter = expresion.pop(0)
-    while parentesis != 0 or len(subcadena2) == 0:
-        if letter == ")":
-            parentesis += -1
-        elif letter == "(":
-            parentesis += 1
-        subcadena2 += letter
-        letter = expresion.pop(0) if len(expresion)>0 else ''
+    if letter == '¬':
+        return not booleana(''.join(expresion))
+    subcadena, simbolo = agrupar(expresion, letter)
+    subcadena2, letter = agrupar(expresion, expresion.pop(0))
     if simbolo == '=':
         return aritmetica(subcadena) == aritmetica(subcadena2)
     elif simbolo == '<':
@@ -73,4 +68,7 @@ def booleana(cadena):
     elif simbolo == '|' or simbolo == '∨':
         return booleana(subcadena) or booleana(subcadena2)
 
-print("Resultado: "+str(booleana(input('Expresion Booleana: ').replace(" ", ''))))
+try:
+    print("Resultado: "+str(booleana(input('Expresion Booleana: ').replace(" ", ''))))
+except (TypeError, IndexError):
+    print("Error: Expresion mal formada, verifique la expresion.")
