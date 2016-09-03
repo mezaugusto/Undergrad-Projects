ec0c584756d32dfab7785b992e8a5df19a096fad
Augusto Meza <amppumas@gmail.com>
2016-09-02 19:14:00 -0500

    Cohen Sutherland Terminado :rose:
    :+1: :rose:

--- compugra/cohensuth.cpp
+++ compugra/cohensuth.cpp
@@ -1,27 +1,25 @@
+using namespace std; 
 #include <stdio.h>
 #include <stdlib.h>
-#define dimensions 2
+#include <fstream>
+#define INPUT_FILE "input.txt"
 
-int defineRegion(float x, float y, float* window)
-{
-	float a,b,c,d;
+
+int defineRegion(int x, int y, int* window){
+	int a,b,c,d;
 	int region = 0;
-	// Lower left point
 	a = window[0];
 	b = window[1];
-	// Upper right point
 	c = window[2];
 	d = window[3];
-	if (x <= a)			region += 8;
-	else if (x >= c)	region += 4;
-	if (y >= d)			region += 2;
-	else if (y <= b)	region += 1;
+	if (x <= a)			region += 2;
+	else if (x >= c)	region += 1;
+	if (y >= d)			region += 8;
+	else if (y <= b)	region += 4;
 	return region;
 }
 
-
-char* cohensuth(float* window,float* line)
-{
+char* cohenSuth(int* window,int* line){
 	int reg1,reg2;
 	char* result;
 	reg1 = defineRegion(line[0],line[1],window);
@@ -32,18 +30,56 @@ char* cohensuth(float* window,float* line)
 	return result;
 }
 
+void readLines(int* window, char * filename){
+	char ch;
+	char str_point[11] = "";
+	int step = 0;
+	int points = 0;
+	int line[4];
+	fstream fin(filename, fstream::in);
+	while (fin >> noskipws >> ch) {
+		if (ch == ',' || ch == '\n') {
+			char *substring = (char *)malloc((step + 1) * sizeof(char));;
+			strncpy(substring, str_point, step);
+			substring[step+1] = '\0';
+			line[points] = (int)strtol(substring, (char **)NULL, 10);
+			points++;
+			step = 0;
+			if (points == 4) {
+				points = 0;
+				printf("La linea de (%d, %d) a (%d, %d) se %s\n", line[0], line[1], line[2], line[3], cohenSuth(window, line));
+			}
+		}
+		else {
+			str_point[step] = ch;
+			step++;
+		}
+	}
+}
+
+int* getWindow() {
+	int* window = (int*)malloc(4 * sizeof(int));
+	printf("\t\tPaso 1\nInserte el punto inferior izquierdo\nX: ");
+	scanf("%d", &window[0]);
+	printf("\nY: ");
+	scanf("%d", &window[1]);
+	printf("\n\t\tPaso 2\nInserte el punto superior derecho\nX: ");
+	scanf("%d", &window[2]);
+	printf("\nY: ");
+	scanf("%d", &window[3]);
+	system("cls");
+	printf("\nResultado\n");
+	for (size_t i = 0; i < 4; i++)
+		if (window[i] < 0) {
+			system("cls");
+			printf("\nERROR: Los numeros deben ser enteros positivos.\n");
+			return getWindow();
+		}
+	return window;
+}
+
 int main(){
-	float *window,*line;
-	window = (float*)calloc(dimensions*2,sizeof(float));
-	line = (float*)calloc(dimensions*2,sizeof(float));
-	window[0] = 5.0;
-	window[1] = 2.0;
-	window[2] = 9.0;
-	window[3] = 7.0;
-	line[0] = 6.0;
-	line[1] = 3.0;
-	line[2] = 8.0;
-	line[3] = 6.0;
-	printf("La linea de (%1.2f, %1.2f) a (%1.2f, %1.2f) se %s\n",line[0],line[1],line[2],line[3],cohensuth(window,line));
+	readLines(getWindow(),INPUT_FILE);
+	system("pause");
 	return 0;
 }
